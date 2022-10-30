8984ef3be95dd6b79ad1e2a7e93c5ac13788d18c
unknown <terrychen8641@gmail.com>
2022-10-30 16:28:13 +0800

    calc avg length of review

--- read.py
+++ read.py
@@ -6,8 +6,10 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 ==0: #每1000筆才印一次，以加速程式
 			print(len(data))
-print(len(data))
+print('檔案讀取完了，總共有', len(data), '筆資料')
 
-print(data[0])
-print('-------------------')
-print(data[1])
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('平均是', sum_len/len(data))
